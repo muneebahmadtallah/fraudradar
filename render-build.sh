393f3c29c6669f619f460b732822eee14d1c123c
muneebahmadtallah <muneebahmad336@gmail.com>
2026-06-12 23:55:10 +0500

    Fix Render build config

--- render-build.sh
+++ render-build.sh
@@ -7,5 +7,4 @@ npm i -g yarn   # Yarn 1.x will be installed
 echo "=== Running original Render commands ==="
 yarn --cwd backend install
 yarn --cwd frontend install
-yarn --cwd frontend run build --prod
-yarn --cwd backend start
+yarn --cwd frontend run build --configuration production
